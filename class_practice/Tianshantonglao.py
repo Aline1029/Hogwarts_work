7f88a4caff7eba4fc0bdbb95a1dfd6046c885981
Mu Shenglin <695134625@qq.com>
2020-08-01 10:45:04 +0800

    作业二 version2.0

--- class_practice/Tianshantonglao.py
+++ class_practice/Tianshantonglao.py
@@ -7,27 +7,33 @@
 
 class TongLao:
     def __init__(self, hp, power):
+        # 添加血量和攻击力属性
         self.hp = hp
         self.power = power
-
+    # 喊人方法
     def see_people(self, name):
+        # 喊不同的名字会有不同的执行
         if name == "无崖子":
             print("师弟！！！！")
         elif name == "李秋水":
             print("呸，贱人")
         elif name == "丁春秋":
             print("叛徒！我杀了你")
-
+    # 天山折梅手
     def fight_zms(self, em_hp, em_power):
+        # 血量减半， 攻击力提升10倍
         self.hp /= 2
         self.power *= 10
+        # 一回合对打
         self.hp -= em_power
         em_hp -= self.power
+        # 血量高的获胜， 血量相等平局
         if self.hp > em_hp:
             print("天山童姥获胜！")
         elif self.hp < em_hp:
             print("敌人获胜！")
         else: print("平局")
+# 实例化
 if __name__ == "__main__":
     tstl = TongLao(1000, 300)
     tstl.see_people("无崖子")
--- class_practice/Xuzhu.py
+++ class_practice/Xuzhu.py
@@ -5,14 +5,16 @@
 # 文件名称    : Xuzhu  PY
 # 开发工具    : PyCharm
 
+# 模块化思想，从隔壁模块中导入类
 from class_practice.Tianshantonglao import TongLao
 
-
+# 构造Xuzhu类，继承自TongLao
 class Xuzhu(TongLao):
+    # 添加独有的念经方法
     def read(self):
         print("罪过罪过")
 
-
+# 实例化
 if __name__ == "__main__":
     xuzhu = Xuzhu(100, 50)
     xuzhu.read()
